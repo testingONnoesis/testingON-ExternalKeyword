46b6b1115a7d748e13e06dae4893239fe8b7c709
NTXnoesis <91883785+NTXnoesis@users.noreply.github.com>
2024-01-31 09:51:27 +0000

    Update Main.java

--- src/com/noesis/ntx/externalkeyword/run/Main.java
+++ src/com/noesis/ntx/externalkeyword/run/Main.java
@@ -18,14 +18,14 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		try{
-			//Export this file to a JAR file and fill the "Secondary Data" field in NTX with the path to the jar
+			//Export this file to a JAR file and fill the "Secondary Data" field in testingON with the path to the jar
 			
 			boolean passed = true;
 			String sessionId = args[0];
 			String url = args[1];
-			String xpath = args [2]; //Xpath received from NTX, if you don't select an object in NTX the "xpath" variable will be "xxxxx"
-			String inputData = args [3]; //Input Data received from NTX, if you don't fill the "Input Data" field in NTX the "inputData" variable will be "xxxxx"
-			String tertiaryData = args [4]; //Tertiary Data received from NTX, if you don't fill the "Tertiary Data" field in NTX the "tertiaryData" variable will be "xxxxx"
+			String xpath = args [2]; //Xpath received from testingON, if you don't select an object in testingON the "xpath" variable will be "xxxxx"
+			String inputData = args [3]; //Input Data received from testingON, if you don't fill the "Input Data" field in testingON the "inputData" variable will be "xxxxx"
+			String tertiaryData = args [4]; //Tertiary Data received from testingON, if you don't fill the "Tertiary Data" field in testingON the "tertiaryData" variable will be "xxxxx"
 			
 			SessionId session_id = new SessionId(sessionId);
 			URL urlSelenium = new URL(url);
